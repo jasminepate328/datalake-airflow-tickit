382954ec31546ddb6dc1fb2d36894e32be21afd2
Jasmine Pate <jasminepate57@gmail.com>
2023-02-21 23:21:22 -0500

    Bronze (raw) data scripts

---
+++ raw_scripts/event_raw.py
@@ -0,0 +1,35 @@
+import sys
+
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME", "s3_bucket"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+event_table = glueContext.create_dynamic_frame.from_catalog(
+    database="tickit",
+    table_name="tickit_saas_event",
+    transformation_ctx="event_table",
+)
+
+S3bucket_node3 = glueContext.getSink(
+    path=f"s3://{args['s3_bucket']}/bronze/event/",
+    connection_type="s3",
+    updateBehavior="UPDATE_IN_DATABASE",
+    partitionKeys=[],
+    enableUpdateCatalog=True,
+    transformation_ctx="S3bucket_node3",
+)
+S3bucket_node3.setCatalogInfo(
+    catalogDatabase="tickit", catalogTableName="raw_tickit_event"
+)
+S3bucket_node3.setFormat("avro")
+S3bucket_node3.writeFrame(event_table)
+job.commit()
---
+++ raw_scripts/users_raw.py
@@ -0,0 +1,35 @@
+import sys
+
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME", "s3_bucket"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+users_table = glueContext.create_dynamic_frame.from_catalog(
+    database="tickit",
+    table_name="tickit_saas_users",
+    transformation_ctx="users_table",
+)
+
+S3bucket_node3 = glueContext.getSink(
+    path=f"s3://{args['s3_bucket']}/bronze/users/",
+    connection_type="s3",
+    updateBehavior="UPDATE_IN_DATABASE",
+    partitionKeys=[],
+    enableUpdateCatalog=True,
+    transformation_ctx="S3bucket_node3",
+)
+S3bucket_node3.setCatalogInfo(
+    catalogDatabase="tickit", catalogTableName="raw_tickit_users"
+)
+S3bucket_node3.setFormat("avro")
+S3bucket_node3.writeFrame(users_table)
+job.commit()
